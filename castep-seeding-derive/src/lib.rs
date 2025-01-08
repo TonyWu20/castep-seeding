6ad8ab5a96458747053c5ddc2d59c1880ad2380a
TonyWu20 <tony.w21@gmail.com>
2025-01-08 16:42:27 +0800

    Implement `ParamDisplay` and `param_display` to derive `Display` on structs with fields of types implemented `KeywordDisplay` or `Display`

--- castep-seeding-derive/src/lib.rs
+++ castep-seeding-derive/src/lib.rs
@@ -1,7 +1,50 @@
-use darling::FromDeriveInput;
+use darling::{FromAttributes, FromDeriveInput};
 use proc_macro::{self, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Ident};
+use syn::{parse_macro_input, DeriveInput, Expr, Ident};
+
+#[derive(FromAttributes, Default)]
+#[darling(default, attributes(param_display))]
+struct ParamFieldOpt {
+    display: Option<Expr>,
+}
+
+#[proc_macro_derive(ParamDisplay, attributes(param_display))]
+pub fn derive_param_display(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = &input.ident;
+    let data = if let syn::Data::Struct(data) = &input.data {
+        data
+    } else {
+        unimplemented!()
+    };
+    let fields = data.fields.iter().map(|f| {
+        let name = f.ident.as_ref().unwrap();
+        let opts = ParamFieldOpt::from_attributes(&f.attrs).expect("Wrong option");
+        let output_func = match opts.display {
+            Some(e) => quote! {
+                #e
+            },
+            None => quote! {
+                output()
+            },
+        };
+        quote! {
+            self.#name.map(|v| v.#output_func)
+        }
+    });
+    let expanded = quote! {
+        impl Display for #ident {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let output = [
+                    #(#fields, )*
+                ].into_iter().flatten().collect::<Vec<String>>().join("\n");
+                write!(f, "{output}")
+            }
+        }
+    };
+    expanded.into()
+}
 
 #[derive(FromDeriveInput, Default)]
 #[darling(default, attributes(keyword_display))]
