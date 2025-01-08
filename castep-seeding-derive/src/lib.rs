02998cdc9e021df046dc145ba21b486ae1b669f9
TonyWu20 <tony.w21@gmail.com>
2025-01-08 12:19:55 +0800

    Add `From` derive for newtype struct

--- castep-seeding-derive/src/lib.rs
+++ castep-seeding-derive/src/lib.rs
@@ -1,12 +1,13 @@
 use darling::FromDeriveInput;
 use proc_macro::{self, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[derive(FromDeriveInput, Default)]
 #[darling(default, attributes(keyword_display))]
 struct Opts {
     field: String,
+    from: Option<Ident>,
 }
 
 #[proc_macro_derive(KeywordDisplay, attributes(keyword_display))]
@@ -21,10 +22,22 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     };
 
+    let from = match opts.from {
+        Some(x) => quote! {
+            impl From<#x> for #ident {
+                fn from(value: #x) -> Self {
+                    Self(value)
+                }
+            }
+        },
+        None => quote! {},
+    };
+
     let output = quote! {
         impl KeywordDisplay for #ident {
             #field_text
         }
+        #from
     };
     output.into()
 }
